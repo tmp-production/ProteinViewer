257f382e843dee7c30f202d7a3715187c308769a
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2021-11-27 20:53:10 +0700

    Implement creating segment & chain meshes

--- Source/ProteinViewer/ribbon/Ribbon.h
+++ Source/ProteinViewer/ribbon/Ribbon.h
@@ -1,9 +1,13 @@
 #pragma once
+
 #include "PeptidePlane.h"
+#include "Triangle.h"
+#include "ProteinViewer/pdb/model/Chain.h"
 
 namespace ribbon
 {
 	typedef TPair<TArray<FVector>, TArray<FVector>> VectorsPair;
+	typedef TPair<FColor, FColor> ColorPair;
 
 	TArray<FVector> ellipseProfile(const int n, const double width, const double height);
 
@@ -16,4 +20,20 @@ namespace ribbon
 	TArray<FVector> translateProfile(const TArray<FVector> p, const double dx, const double dy);
 
 	VectorsPair segmentProfiles(const PeptidePlane& pp1, const PeptidePlane& pp2, const int n);
+
+	ColorPair segmentColors(const PeptidePlane& pp);
+
+	void triangulateQuad(
+		TArray<Triangle>& triangles,
+		const FVector& p1, const FVector& p2, const FVector& p3, const FVector& p4,
+		const FColor& c1, const FColor& c2, const FColor& c3, const FColor& c4
+	);
+
+	TArray<Triangle> createSegmentMesh(
+		int i, const int n,
+		const PeptidePlane& pp1, const PeptidePlane& pp2,
+		const PeptidePlane& pp3, const PeptidePlane& pp4
+	);
+
+	TArray<Triangle> createChainMesh(const pdb::Chain& chain);
 }

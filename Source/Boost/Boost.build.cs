786aee1eab602c9360e4444ce4935c503bd34474
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2022-03-26 15:09:00 +0700

    Add the Boost library source files
    Source: https://www.boost.org/users/download/

---
+++ Source/Boost/Boost.build.cs
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Novosibirsk State University. All rights reserved.
+
+using UnrealBuildTool;
+
+public class Boost : ModuleRules
+{
+	public Boost(ReadOnlyTargetRules Target) : base(Target)
+	{
+	}
+}

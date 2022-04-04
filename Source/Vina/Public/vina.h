d070f5ca447cbe26afab84262f02baa29c193aea
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2022-04-04 19:48:50 +0700

    Add documentation for the Vina API

--- Source/Vina/Public/vina.h
+++ Source/Vina/Public/vina.h
@@ -1,57 +1,149 @@
 #pragma once
 
-#include <stdlib.h>
+#include <cstdlib>
 #include <string>
 #include <vector>
 
 #ifdef VINA_API
 #undef VINA_API
-#define VINA_API __attribute__((visibility("default")))
 #endif
 
-// TODO: Add documentation to the Vina instance methods
-// https://github.com/ccsb-scripps/AutoDock-Vina/blob/develop/build/python/vina/vina.py
+#define VINA_API __attribute__((visibility("default")))
 
 class VINA_API Vina {
 public:
-    Vina(const std::string &sf_name="vina", int cpu=0, int seed=0, int verbosity=1, bool no_refine=false, std::function<void(double)>* progress_callback = NULL);
+    /// Initialize a Vina object.
+    /// \param sf_name Scoring function name to use (Vina or ad4) (default: vina)
+    /// \param cpu Number of CPU to use (default: 0; use all of them)
+    /// \param seed Random seed (default: 0; randomly chosen)
+    /// \param verbosity verbosity 0: not output, 1: normal, 2: verbose (default: 1; some output)
+    /// \param no_refine when receptor is provided, do not use explicit receptor atoms
+    /// (instead of precalculated grids) for: (1) local optimization and scoring after docking,
+    /// (2) --local_only jobs, and (3) --score_only jobs (default: False)
+    explicit Vina(const std::string &sf_name = "vina", int cpu = 0, int seed = 0, int verbosity = 1,
+                  bool no_refine = false, std::function<void(double)> *progress_callback = nullptr);
+
+    /// Deinitialize a Vina object.
     ~Vina();
-    
-    void cite();
-    int seed();
-    
-    void set_receptor(const std::string &rigid_name=std::string(), const std::string &flex_name=std::string());
+
+    /// Set receptor.
+    /// \param rigid_name rigid pdbqt receptor filename
+    /// \param flex_name flexible residues pdbqt filename
+    void set_receptor(const std::string &rigid_name = std::string(), const std::string &flex_name = std::string());
+
+    /// Set ligand from a string. The chemical file format must be PDBQT.
+    /// \param ligand_string PDBQT string
     void set_ligand_from_string(const std::string &ligand_string);
+
+    /// Set ligands from a string. The chemical file format must be PDBQT.
+    /// \param ligand_string vector of PDBQT strings
     void set_ligand_from_string(const std::vector<std::string> &ligand_string);
-    void set_ligand_from_file(const std::string& ligand_name);
-    void set_ligand_from_file(const std::vector<std::string>& ligand_name);
-	void set_vina_weights(double weight_gauss1=-0.035579, double weight_gauss2=-0.005156,
-						       double weight_repulsion=0.840245, double weight_hydrophobic=-0.035069,
-						       double weight_hydrogen=-0.587439, double weight_glue=50,
-						       double weight_rot=0.05846);
-	void set_vinardo_weights(double weight_gauss1=-0.045,
-							       double weight_repulsion=0.8, double weight_hydrophobic=-0.035,
-							       double weight_hydrogen=-0.600, double weight_glue=50,
-							       double weight_rot=0.05846);
-	void set_ad4_weights(double weight_ad4_vdw=0.1662, double weight_ad4_hb=0.1209,
-						      double weight_ad4_elec=0.1406, double weight_ad4_dsolv=0.1322,
-						      double weight_glue=50, double weight_ad4_rot=0.2983);
-	void compute_vina_maps(double center_x, double center_y, double center_z,
-								  double size_x, double size_y, double size_z,
-								  double granularity=0.5, bool force_even_voxels=false);
-	void load_maps(std::string maps);
-	void randomize(const int max_steps=10000);
-    std::vector<double> score();
-    std::vector<double> optimize(const int max_steps=0);
-    void global_search(const int exhaustiveness=8, const int n_poses=20, const double min_rmsd=1.0, const int max_evals=0);
-    std::string get_poses(int how_many=9, double energy_range=3.0);
-    std::vector< std::vector<double> > get_poses_coordinates(int how_many=9, double energy_range=3.0);
-    std::vector< std::vector<double> > get_poses_energies(int how_many=9, double energy_range=3.0);
+
+    /// Set ligand from a file. The chemical file format must be PDBQT.
+    /// \param ligand_name PDBQT filename
+    void set_ligand_from_file(const std::string &ligand_name);
+
+    /// Set ligands from a file. The chemical file format must be PDBQT.
+    /// \param ligand_name vector of PDBQT filenames
+    void set_ligand_from_file(const std::vector<std::string> &ligand_name);
+
+    /// Set potential weights for vina scoring function.
+    void set_vina_weights(double weight_gauss1 = -0.035579, double weight_gauss2 = -0.005156,
+                          double weight_repulsion = 0.840245, double weight_hydrophobic = -0.035069,
+                          double weight_hydrogen = -0.587439, double weight_glue = 50,
+                          double weight_rot = 0.05846);
+
+    /// Set potential weights for vinardo scoring function.
+    void set_vinardo_weights(double weight_gauss1 = -0.045,
+                             double weight_repulsion = 0.8, double weight_hydrophobic = -0.035,
+                             double weight_hydrogen = -0.600, double weight_glue = 50,
+                             double weight_rot = 0.05846);
+
+    /// Set potential weights for ad4 scoring function.
+    void set_ad4_weights(double weight_ad4_vdw = 0.1662, double weight_ad4_hb = 0.1209,
+                         double weight_ad4_elec = 0.1406, double weight_ad4_dsolv = 0.1322,
+                         double weight_glue = 50, double weight_ad4_rot = 0.2983);
+
+    /// Compute affinity maps using Vina scoring function.
+    /// \param center_x center position (X)
+    /// \param center_y center position (Y)
+    /// \param center_z center position (Z)
+    /// \param size_x size of the box in Angstrom (X)
+    /// \param size_y size of the box in Angstrom (Y)
+    /// \param size_z size of the box in Angstrom (Z)
+    /// \param granularity grid spacing (default: 0.5)
+    /// \param force_even_voxels Force the number of voxels (NPTS/NELEMENTS) to be an even number
+    /// (and forcing the number of grid points to be odd) (default: false)
+    void compute_vina_maps(double center_x, double center_y, double center_z,
+                           double size_x, double size_y, double size_z,
+                           double granularity = 0.5, bool force_even_voxels = false);
+
+    /// Load vina or ad4 affinity maps.
+    /// \param maps affinity map prefix filename
+    void load_maps(const std::string &maps);
+
+    /// Write affinity maps.
+    /// \param map_prefix affinity map pathname (path directory + prefix)
+    /// \param gpf_filename grid protein filename (default: NULL)
+    /// \param fld_filename fld filename (default: NULL)
+    /// \param receptor_filename receptor filename (default: NULL)
+    void write_maps(const std::string &map_prefix = "receptor", const std::string &gpf_filename = "NULL",
+                    const std::string &fld_filename = "NULL", const std::string &receptor_filename = "NULL");
+
+    /// Write pose (after randomize or optimize).
+    /// \param output_name output PDBQT filename
+    /// \param remark REMARKS to add in the PDBQT filename
     void write_pose(const std::string &output_name, const std::string &remark = std::string());
-    void write_poses(const std::string &output_name, int how_many=9, double energy_range=3.0);
-    void write_maps(const std::string& map_prefix="receptor", const std::string& gpf_filename="NULL",
-                        const std::string& fld_filename="NULL", const std::string& receptor_filename="NULL");
-    void show_score(const std::vector<double> energies);
+
+    /// Write poses from docking.
+    /// \param output_name PDBQT file containing poses found
+    /// \param how_many number of poses to write (default: 9)
+    /// \param energy_range maximum energy difference from best pose (default: 3.0 kcal/mol)
+    void write_poses(const std::string &output_name, int how_many = 9, double energy_range = 3.0);
+
+    /// Get poses from docking.
+    /// \param how_many number of poses to retrieve (default: 9)
+    /// \param energy_range maximum energy difference from best pose (default: 3.0 kcal/mol)
+    /// \return PDBQT file strings
+    std::string get_poses(int how_many = 9, double energy_range = 3.0);
+
+    /// Get poses from docking.
+    /// \param how_many number of poses to retrieve (default: 9)
+    /// \param energy_range maximum energy difference from best pose (default: 3.0 kcal/mol)
+    /// \return vector of coordinates of poses
+    std::vector<std::vector<double>> get_poses_coordinates(int how_many = 9, double energy_range = 3.0);
+
+    /// Get pose energies from docking.
+    /// \param how_many number of poses to retrieve (default: 9)
+    /// \param energy_range maximum energy difference from best pose (default: 3.0 kcal/mol)
+    /// \return vector of energies from each pose (rows=poses, columns=energies)\n
+    /// Vina/Vinardo FF: columns=[total, inter, intra, torsions, intra best pose]\n
+    /// AutoDock FF: columns=[total, inter, intra, torsions, -intra]
+    std::vector<std::vector<double>> get_poses_energies(int how_many = 9, double energy_range = 3.0);
+
+    /// Randomize the input ligand conformation.
+    void randomize(int max_steps = 10000);
+
+    /// Score current pose.
+    /// \return vector of energies from current pose.\n
+    /// Vina/Vinardo FF: columns=[total, lig_inter, flex_inter, other_inter, flex_intra, lig_intra, torsions, lig_intra best pose]\n
+    /// AutoDock FF: columns=[total, lig_inter, flex_inter, other_inter, flex_intra, lig_intra, torsions, -lig_intra]
+    std::vector<double> score();
+
+    /// Quick local BFGS energy optimization.
+    /// \param max_steps Maximum number of local minimization steps (default: 0). When max_steps is equal to 0,
+    /// the maximum number of steps will be equal to (25 + num_movable_atoms) / 3).
+    /// \return Array of energies from optimized pose.\n
+    /// Vina/Vinardo FF: columns=[total, lig_inter, flex_inter, other_inter, flex_intra, lig_intra, torsions, lig_intra best pose]\n
+    /// AutoDock FF: columns=[total, lig_inter, flex_inter, other_inter, flex_intra, lig_intra, torsions, -lig_intra]
+    std::vector<double> optimize(int max_steps = 0);
+
+    /// Docking: global search optimization.
+    /// \param exhaustiveness Number of MC run (default: 8)
+    /// \param n_poses number of pose to generate (default: 20)
+    /// \param min_rmsd minimum RMSD difference between poses (default: 1.0 Ansgtrom)
+    /// \param max_evals Maximum number of evaluation (default: 0; use heuristics rules)
+    void global_search(int exhaustiveness = 8, int n_poses = 20, double min_rmsd = 1.0, int max_evals = 0);
 
 private:
     void *vina_impl;
--- Source/Vina/vina_wrapper.cpp
+++ Source/Vina/vina_wrapper.cpp
@@ -1,7 +1,8 @@
 #include "Public/vina.h"
 #include "Private/vina.h"
 
-Vina::Vina(const std::string &sf_name, int cpu, int seed, int verbosity, bool no_refine, std::function<void(double)>* progress_callback) {
+Vina::Vina(const std::string &sf_name, int cpu, int seed, int verbosity, bool no_refine,
+           std::function<void(double)> *progress_callback) {
     vina_impl = new IMPL::Vina(sf_name, cpu, seed, verbosity, no_refine, progress_callback);
 }
 
@@ -9,14 +10,111 @@ Vina::~Vina() {
     delete ((IMPL::Vina *) vina_impl);
 }
 
-void Vina::cite() {
-    auto vina = (IMPL::Vina *) vina_impl;
-    vina->cite();
+void Vina::set_receptor(const std::string &rigid_name, const std::string &flex_name) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->set_receptor(rigid_name, flex_name);
 }
 
-int Vina::seed() {
-    auto vina = (IMPL::Vina *) vina_impl;
-    return vina->seed();
-};
+void Vina::set_ligand_from_string(const std::string &ligand_string) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->set_ligand_from_string(ligand_string);
+}
 
-// TODO: Implement remaining methods from "Public/vina.h"
+void Vina::set_ligand_from_string(const std::vector<std::string> &ligand_string) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->set_ligand_from_string(ligand_string);
+}
+
+void Vina::set_ligand_from_file(const std::string &ligand_name) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->set_ligand_from_file(ligand_name);
+}
+
+void Vina::set_ligand_from_file(const std::vector<std::string> &ligand_name) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->set_ligand_from_file(ligand_name);
+}
+
+void Vina::set_vina_weights(double weight_gauss1, double weight_gauss2, double weight_repulsion,
+                            double weight_hydrophobic, double weight_hydrogen, double weight_glue, double weight_rot) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->set_vina_weights(weight_gauss1, weight_gauss2, weight_repulsion,
+                           weight_hydrophobic, weight_hydrogen, weight_glue, weight_rot);
+}
+
+void Vina::set_vinardo_weights(double weight_gauss1, double weight_repulsion, double weight_hydrophobic,
+                               double weight_hydrogen, double weight_glue, double weight_rot) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->set_vinardo_weights(weight_gauss1, weight_repulsion, weight_hydrophobic,
+                              weight_hydrogen, weight_glue, weight_rot);
+}
+
+void Vina::set_ad4_weights(double weight_ad4_vdw, double weight_ad4_hb, double weight_ad4_elec,
+                           double weight_ad4_dsolv, double weight_glue, double weight_ad4_rot) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->set_ad4_weights(weight_ad4_vdw, weight_ad4_hb, weight_ad4_elec,
+                          weight_ad4_dsolv, weight_glue, weight_ad4_rot);
+}
+
+void Vina::compute_vina_maps(double center_x, double center_y, double center_z,
+                             double size_x, double size_y, double size_z,
+                             double granularity, bool force_even_voxels) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->compute_vina_maps(center_x, center_y, center_z, size_x, size_y, size_z, granularity, force_even_voxels);
+}
+
+void Vina::load_maps(const std::string &maps) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->load_maps(maps);
+}
+
+void Vina::write_maps(const std::string &map_prefix, const std::string &gpf_filename,
+                      const std::string &fld_filename, const std::string &receptor_filename) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->write_maps(map_prefix, gpf_filename, fld_filename, receptor_filename);
+}
+
+void Vina::write_pose(const std::string &output_name, const std::string &remark) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->write_pose(output_name, remark);
+}
+
+void Vina::write_poses(const std::string &output_name, int how_many, double energy_range) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->write_poses(output_name, how_many, energy_range);
+}
+
+std::string Vina::get_poses(int how_many, double energy_range) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    return vina->get_poses(how_many, energy_range);
+}
+
+std::vector<std::vector<double>> Vina::get_poses_coordinates(int how_many, double energy_range) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    return vina->get_poses_coordinates(how_many, energy_range);
+}
+
+std::vector<std::vector<double>> Vina::get_poses_energies(int how_many, double energy_range) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    return vina->get_poses_energies(how_many, energy_range);
+}
+
+void Vina::randomize(int max_steps) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->randomize(max_steps);
+}
+
+std::vector<double> Vina::score() {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    return vina->score();
+}
+
+std::vector<double> Vina::optimize(int max_steps) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    return vina->optimize(max_steps);
+}
+
+void Vina::global_search(int exhaustiveness, int n_poses, double min_rmsd, int max_evals) {
+    const auto vina = (IMPL::Vina *) vina_impl;
+    vina->global_search(exhaustiveness, n_poses, min_rmsd, max_evals);
+}
